5eb1feb6626caf5ecb5fdc72f120fc5abec83b0b
Darshan Patel <patel.dars@husky.neu.edu>
2016-11-04 19:26:52 -0400

    PageRank until first iteration is completed

--- PageRankSpark/src/main/scala/Parser.scala
+++ PageRankSpark/src/main/scala/Parser.scala
@@ -1,7 +1,16 @@
+import java.io.StringReader
+
 import org.apache.spark.rdd.RDD._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import java.util
+import java.util.regex.{Matcher, Pattern}
+import javax.xml.parsers.{SAXParser, SAXParserFactory}
+
+import org.xml.sax.InputSource
+
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 
@@ -9,6 +18,144 @@ import scala.collection.JavaConverters._
   * Created by Darshan on 11/1/16.
   */
 object Parser {
+
+  def isGoodName(line : String): Boolean ={
+
+    val namePattern = Pattern.compile("^([^~]+)$")
+    val questionPattern = Pattern.compile("^[? ]*$")
+
+    val delimLoc: Int = line.indexOf(':')
+    val pageName: String = line.substring(0, delimLoc)
+    val matcher: Matcher = namePattern.matcher(pageName)
+    val questionMatcher: Matcher = questionPattern.matcher(pageName)
+
+    //if (!matcher.find() || !specialCharMatcher.find())
+    if (!matcher.find || questionMatcher.find) {
+      // Skip this html file, name contains (~).
+      false
+    }
+    true
+  }
+
+  def initialParser(input : String, sc : SparkContext) : RDD[(String, (String, util.List[String]))] = {
+
+    //Read the input files and parse them.
+    val lines = sc.textFile(input).filter(line => isGoodName(line)).
+    map(line => {
+    val node = bz2WikiParser(line)
+    (node.getPageName, node.getAdjPages)
+    }).keyBy{line => line._1}
+
+    //lines.saveAsTextFile("./parseoutput")
+
+    // Convert the Dead nodes into Dangling nodes
+    val dummyLines = lines.flatMap(line => {
+    val adjPages = line._2._2
+    //adjPages.asScala.toList.foreach(page => (page, emptyAdjPages))
+    val newArray = for (value <- adjPages) yield {
+    val emptyAdjPages =  List[String]().asJava
+    (value, emptyAdjPages)
+    }
+    newArray
+    }).keyBy{ line => line._1}
+
+    //dummyLines.saveAsTextFile("./dummyLines")
+
+    // Convert the multiple empty list as value to single empty list
+    val reducedDummyLines = dummyLines.reduceByKey((a, b) => a)
+
+    // Union the all the pages with initially parsed pages.
+    val allPages = lines.union(reducedDummyLines)
+
+    val reducedAllPages = allPages.reduceByKey({(a, b) =>
+    if(a._2.length == 0){
+    b
+    }else{
+      a
+    }
+    })
+    reducedAllPages
+  }
+
+  def bz2WikiParser(line : String): Node ={
+    // Configure parser
+    var linkPageNames: util.List[String] = new util.LinkedList[String]
+    val node: Node = new Node
+
+    val spf: SAXParserFactory = SAXParserFactory.newInstance
+    spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
+    val saxParser: SAXParser = spf.newSAXParser
+
+    val xmlReader = saxParser.getXMLReader
+    linkPageNames = new util.LinkedList[String]
+    xmlReader.setContentHandler(new WikiParser(linkPageNames))
+
+    val delimLoc: Int = line.indexOf(':')
+    val pageName: String = line.substring(0, delimLoc)
+    var html: String = line.substring(delimLoc + 1)
+
+    // Parse page and fill list of linked pages.
+    try {
+      html = html.replace("&", "&amp;")
+      xmlReader.parse(new InputSource(new StringReader(html)))
+    } catch {
+      case e: Exception => {
+        // Discard ill-formatted pages.
+        linkPageNames.clear()
+      }
+    }
+    val pageNamesSet: util.Set[String] = new util.HashSet[String](linkPageNames)
+    // Remove source page name from its adjacency list if exists.
+    pageNamesSet.remove(pageName)
+    linkPageNames = new util.LinkedList[String](pageNamesSet)
+
+    node.setPageName(pageName)
+    node.setAdjPages(linkPageNames)
+
+    node
+  }
+
+  def distributePageRank(pageInfo : RDD[(String, (util.List[String], Double))]): RDD[(String, Double)]={
+
+    val prDistribution = pageInfo.flatMapValues( value => {
+      val adjPages = value._1
+      val pageRank = value._2
+      val adjPageCount = adjPages.length
+      val pageRankDist = for (adjPage <- adjPages) yield {
+      (1.0/(pageRank*adjPageCount))
+      }
+      pageRankDist
+    })
+
+    val reducedPRDistribution = prDistribution.reduceByKey({(a,b) => a+b})
+    reducedPRDistribution
+  }
+
+  def calculateNewPageRank(distAdjPagePR : RDD[(String, Double)],
+                           allPages : RDD[(String, (util.List[String], Double))],
+                           pageCount : Long,
+                           delta: Double,
+                           sc : SparkContext) : (RDD[(String, (util.List[String], Double))], Double) ={
+
+    val pagesWithContributionPR = allPages.leftOuterJoin(distAdjPagePR)
+    val alpha = 0.15d
+    val dangPRSum = sc.doubleAccumulator("Sum Of Dangling Node Page Rank")
+    val newPagePR = pagesWithContributionPR.mapValues( value => {
+
+      val adjPages = value._1._1
+      val curretnPageRank = value._1._2
+      val prContribSum = value._2
+      var newPageRank = 0.0
+      if(prContribSum != None){
+        newPageRank = (alpha/pageCount) + (1-alpha)*(prContribSum.get)
+      }
+      if(adjPages.length == 0){
+        dangPRSum.add(newPageRank)
+      }
+      (adjPages, newPageRank)
+    })
+    (newPagePR, dangPRSum.value)
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -20,149 +167,26 @@ object Parser {
     val conf = new SparkConf().setAppName("PageRank").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val lines = sc.textFile(args(0)).filter(line => Bz2WikiParser.goodPageName(line)).
-      map(line => {
-        val node = Bz2WikiParser.parseHTMLPage(line)
-        (node.getPageName, node.getAdjPages)
-      }).keyBy{line => line._1}
-
-    //lines.saveAsTextFile("./parseoutput")
-
-    /*
-    val upates = lines.flatMap(line => {
-      val length = line._2.size()
-      val values = for(i <- 0 until 1) yield {
-        if(i==0){
-          (line._1, line._2)
-        }else{
-          val emptyAdjPages =  List[String]().asJava
-          for(j <- 0 until length) yield {
-            (line._2.get(j), emptyAdjPages)
-          }
-        }
-      }
-      values.flatten
-    })
-    upates.saveAsTextFile("./updates")
-    */
-
-
-    val dummyLines = lines.flatMap(line => {
-      //val pageName = line._2._1
-      val adjPages = line._2._2
-      //adjPages.asScala.toList.foreach(page => (page, emptyAdjPages))
-      val newArray = for (value <- adjPages) yield {
-        val emptyAdjPages =  List[String]().asJava
-        (value, emptyAdjPages)
-      }
-      newArray
-    }).keyBy{ line => line._1}
-
-    //dummyLines.saveAsTextFile("./dummyLines")
-
-    val reducedDummyLines = dummyLines.reduceByKey((a, b) => a)
-
-    val allPages = lines.union(reducedDummyLines)
-
-    val reducedAllPages = allPages.reduceByKey({(a, b) =>
-        if(a._2.length == 0){
-          b
-        }else{
-          a
-        }
-    })
+    val allPages = initialParser(args(0), sc)
 
     val nbrOfPages = sc.longAccumulator("Total Number of pages")
-    reducedAllPages.foreach(x => nbrOfPages.add(1))
-    val pages = nbrOfPages.value
+    allPages.foreach(x => nbrOfPages.add(1))
+    val pageCount = nbrOfPages.value
 
-    //reducedAllPages.saveAsTextFile("./reducedAllPages")
+    var allPagesWithPR = allPages.mapValues(value => (value._2, 1.0d/pageCount))
 
-    val pageWithPRContrib = reducedAllPages.flatMap( line => {
-      val adjPages = line._2._2
-      //var isDangling = false
-      //if(adjPages.length == 0){
-      //  isDangling = true
-      //}
-      val newArray = for (value <- adjPages) yield {
-        (value, 1.0d/pages)
-      }
-      newArray
-    }).keyBy{line => line._1}
+    //println(distAdjPagePR.count())
 
 
-    val reducedPagePR = pageWithPRContrib.reduceByKey({(a,b) =>
-      (a._1, a._2 + b._2)
-    })
+    var delta : Double = 0.0
 
-    println("dummyLines : " + dummyLines.count())
-    println("nbr of pages " + pages)
-    println("pageWithPRContrib : " + pageWithPRContrib.count())
-    println("reducedPagePR : " + reducedPagePR.count())
-    reducedPagePR.saveAsTextFile("./reducedPagePR")
-    // First Iteration of Page Rank
-
-    //val linesWithPR = reducedAllPages.join(reducedPagePR)
-    //reductedunionLines.saveAsTextFile("./reductedunionLines")
-
-    // val finalResults = reduceDummyLines ++ lines
-    //finalResults.saveAsTextFile("./firstoutput")
+    val distAdjPagePR = distributePageRank(allPagesWithPR)
+    val returnValues = calculateNewPageRank(distAdjPagePR, allPagesWithPR, pageCount, delta, sc)
+    allPagesWithPR = returnValues._1
+    delta = returnValues._2
 
 
-    /*
 
-     val results = lines.flatMap(line => {
-
-      val parts = line.split(":")
-      val key = parts(0)
-      val value = ""
-      val emptyString = ""
-
-      if(parts.length > 1){
-        val value = parts(1).toString
-        val argParts = value.split(",")
-        val doubleSeq = for(j <- 0 until argParts.length) yield {
-          val key = argParts(j).toString
-          (key, emptyString)
-        }
-        doubleSeq.flatten
-      }else{
-        (key, emptyString)
-      }
-      //doubleSeq.flatten
-      //(key.toString, value.toString)
-    })
-        results.keyBy((a, b) => {a}).reduceByKey((a, b) => {
-          if(a._2.length > 1){
-            a._2
-          }else if(b._2.length > 1){
-            b._2
-          }else{
-            a._2
-          }
-        })
-
-
-        val data = sc.parallelize(List("1,2,3", "4,5,6", "7,8,9"))
-        val result = data.flatMap(line=>{
-          val arr = line.split(",")
-          val doubleSeq = for(i <- 0 until arr.length) yield {
-            val x = arr(i).toDouble
-            for(j <- (i+1) until arr.length) yield {
-              val y = arr(j).toDouble
-              val k = Index(i,j)
-              val v = Val(x,y)
-              (k,v)
-            }
-          }
-          doubleSeq.flatten
-        })
-        */
-
-    //val keyedLines = linesU.keyBy((key, value) => key)
-
-    //keyedLines.saveAsTextFile("./firstoutput")
-    //results.saveAsTextFile("./firstoutput")
   }
 
 }
